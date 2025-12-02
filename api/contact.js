a5c3509de9bdfbe25fecd08b4d14b74849681bb6
Chris Thorn <Chris@securely.io>
2025-12-02 08:03:53 -0500

    update email

--- api/contact.js
+++ api/contact.js
@@ -34,7 +34,7 @@ export default async function handler(req, res) {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        from: 'onboarding@resend.dev',
+        from: 'contact@thornwebsolutions.com',
         to: 'christopherthornn@gmail.com',
         subject: subject,
         html: htmlContent,
